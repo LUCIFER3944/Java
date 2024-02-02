51a2ad5997c193fad5d18f9b40fc627848f84bde
Lucifer <vivekrai3944@gmail.com>
2024-02-02 15:35:04 +0530

    update

---
+++ HotelReservation.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class HotelReservation {
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+     System.out.println("Enter your customer name:");
+        String Cname = sc.nextLine();
+        
+        System.out.println("Enter your customer Address:");
+        String address = sc.nextLine();
+        
+        System.out.println("Enter your Mobile number:");
+        long mobileNumber = sc.nextLong();
+
+
+        System.out.println("Choose a room: \n 1. delux,\n 2. normal,\n 3. classic,\n 4. smallroom, \n 5. Bigroom");
+        int room = sc.nextInt();
+
+        System.out.println("Price");
+        int price = sc.nextInt();
+ 
+
+        System.out.println("Enter quantity:");
+        int quantity = sc.nextInt();
+        double totalPrice = 0;
+       
+        switch (room) {
+            case 1:
+                System.out.println("You chose delux.");
+                totalPrice = quantity * price;
+                break;
+            case 2:
+                System.out.println("You chose normal.");
+                totalPrice = quantity * price;
+                break;
+            case 3:
+                System.out.println("You chose classic.");
+                totalPrice = quantity * price;
+                break;
+            case 4:
+                System.out.println("You chose smallroom.");
+                totalPrice = quantity * price;
+                break;
+            case 5:
+                System.out.println("You chose bigroom");
+                totalPrice = quantity * price;
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid pizza.");
+                return;
+        }
+
+        System.out.println("Costumer name="+Cname);
+        System.out.println("Address="+address);
+        System.out.println("Costumer mobile number="+mobileNumber);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("price " + price);
+        System.out.println("Total Price: $" + totalPrice);
+}    
+}
