31c0d21e651e1d18bedc4eb8ce67f1fea3e22b28
Lucifer <vivekrai3944@gmail.com>
2024-02-06 15:25:28 +0530

    update

---
+++ randomNumber.java
@@ -0,0 +1,10 @@
+import java.lang.Math;   
+public class randomNumber 
+{   
+public static void main(String args[])   
+{   
+// Generating random numbers  
+System.out.println("1st Random Number: " + Math.random());   
+  
+}   
+}  
